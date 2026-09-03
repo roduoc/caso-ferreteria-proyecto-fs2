b2aee33402289cc6b53efa4bcd0d6ce62e0b5776
Chimonin <ai.ceron@duocuc.cl>
2026-09-02 21:07:49 -0400

    subida de clientes de prueba para el localstorage

---
+++ assets/js/clientes_prueba.js
@@ -0,0 +1,23 @@
+window.clientesPrueba = [
+  {
+    id: 1,
+    correo: "juan.perez@gmail.com",
+    tipoCliente: "particular",
+    cuentaCorrienteHabilitada: false,
+    saldoActual: 0,
+  },
+  {
+    id: 2,
+    correo: "maria.gonzalez@gmail.com",
+    tipoCliente: "contratista",
+    cuentaCorrienteHabilitada: true,
+    saldoActual: 0,
+  },
+  {
+    id: 3,
+    correo: "contacto@constructorarojas.cl",
+    tipoCliente: "contratista",
+    cuentaCorrienteHabilitada: true,
+    saldoActual: 128500,
+  },
+];
