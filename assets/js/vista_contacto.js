87394e5f501af68747a8b7613db7f8d277065979
Chimonin <ai.ceron@duocuc.cl>
2026-09-05 16:09:03 -0400

    agregando validacion de campos js

--- assets/js/vista_contacto.js
+++ assets/js/vista_contacto.js
@@ -18,10 +18,28 @@ mobileMenu.addEventListener("click", (event) => {
 });
 
 //////////////////////mapa
-// --- Mapa ---
-// Mismas coordenadas y estilo que en vista_envios
 const mapaNosotros = L.map('mapa-nosotros').setView([-29.9027, -71.2519], 15);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mapaNosotros);
 L.marker([-29.9027, -71.2519]).addTo(mapaNosotros)
   .bindPopup('Ferretería Los Maestros')
   .openPopup();
+
+////////////////////validar form
+const formContacto = document.querySelector("#form-contacto");
+formContacto.addEventListener("submit", (event) => {
+    //por defecto, al hacer submit recarga la pagina, pero no queremos eso esta vez
+  event.preventDefault();
+
+  const correo = document.querySelector("#contacto-correo").value.trim();
+  const mensaje = document.querySelector("#contacto-mensaje").value.trim();
+  const errorEl = document.querySelector("#contacto-error");
+
+  if (!correo || !mensaje) {
+    errorEl.textContent = "Debes completar correo y mensaje.";
+    errorEl.classList.remove("hidden");
+    return;
+  }
+
+  errorEl.classList.add("hidden");
+  formContacto.reset();
+});
